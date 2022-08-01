54966bd3daeafa377d3fc6479f841709cfb1389b
jreyess11 <josegabrielreyessolanojr@gmail.com>
2022-08-01 15:04:16 -0500

    Update T1E10.py

--- T1E10.py
+++ T1E10.py
@@ -9,15 +9,18 @@ N = int(input("Ingrese la cantidad de bultos: "))
 total = 0
 i = 0
 tabla = []
-info=[]
 controlpeso=[]
-controlprecio=[]
+info = []
+controlpeso = []
+controlprecio = []
+
 
 def mytable(arr):
-    titulos.field_names=["Bulto #","Peso","Precio COP", "Precio USD"]
+    titulos.field_names = ["Bulto #", "Peso", "Precio COP", "Precio USD"]
     for i in arr:
         titulos.add_row(i)
     return titulos
+
 
 def tarifa(num):
     COP = 0
@@ -30,26 +33,26 @@ def tarifa(num):
 
 while total < N:
     peso = int(input(f"Ingrese el peso del bulto #{i + 1} (kg): "))
-    info=[]
+    info = []
     if peso > 0:
         if peso <= 500:
-            info.append(i+1)
+            info.append(i + 1)
             info.append(peso)
             controlpeso.append(peso)
             if sum(controlpeso) <= 18000:
                 C = tarifa(peso)
-                U = (C/4422.30)
+                U = (C / 4422.30)
                 info.append(f"${C:,.0f}")
                 info.append(f"${U:,.2f}")
                 controlprecio.append(C)
                 i = i + 1
                 total = total + 1
             else:
-                controlpeso.remove(controlpeso[len(controlpeso)-1])
+                controlpeso.remove(controlpeso[len(controlpeso) - 1])
                 info.clear()
                 des = int(input("\nLo sentimos, este bulto excede el tamaño máximo de carga (18.000 kg). "
-                                 f"Por el momento su peso es {sum(controlpeso)} kg. \n"
-                                 "Oprima 1 si desea agregar otro bulto de menor tamaño o 0 para terminar la lista: "))
+                                f"Por el momento su peso es {sum(controlpeso)} kg. \n"
+                                "Oprima 1 si desea agregar otro bulto de menor tamaño o 0 para terminar la lista: "))
                 if des == 0:
                     total = N
                 else:
@@ -61,10 +64,10 @@ while total < N:
     if info:
         tabla.append(info)
 
-print("\n--CONTROL DE DATOS--\n"+str(mytable(tabla)))
+print("\n--CONTROL DE DATOS--\n" + str(mytable(tabla)))
 print(f"\nEl peso final de la carga es {sum(controlpeso)} kg, con un total de {len(controlpeso)} bultos.")
 print(f"El peso del bulto más pesado es {max(controlpeso)} kg y del menor es {min(controlpeso)} kg.")
 print(f"El peso promedio de los bultos es {statistics.mean(controlpeso)} kg.")
-print(f"El precio final de la carga es COP ${sum(controlprecio):,.0f} // USD ${sum(controlprecio)/4422.30:,.2f}")
+print(f"El precio final de la carga es COP ${sum(controlprecio):,.0f} // USD ${sum(controlprecio) / 4422.30:,.2f}")
 
 msvcrt.getch()
